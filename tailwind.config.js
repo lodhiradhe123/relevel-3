53a45b782a2392537f968c15d9baa17a12391263
Radheshyam Lodhi <radheshyamlodhi48@gmail.com>
2024-05-02 14:14:28 +0530

    relevel-3 with backend CRUD operation

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.ejs",
+"./public/stylesheets/style**/*.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
